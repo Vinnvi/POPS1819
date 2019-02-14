54d0c5a8e91f85c536b7c61811ec457fb3b2672f
Quentin Jardet <quentin.jardet@u-psud.fr>
2019-02-14 16:59:52 +0100

    mise en place d'un tableau de notification

---
+++ src/Migrations/Version20190213170238.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190213170238 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE ligne_de_frais ADD last_modif DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE note_de_frais ADD last_modif DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE notification DROP personnel, DROP cumulable, DROP vu');
+    }
+}
